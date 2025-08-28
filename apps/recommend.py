ffa57a3131436f14ce86c5b9c8b845cf4b268e2e
Habeba Mostafa <Habiba.Mostafa.55@h-eng.helwan.edu.eg>
2025-08-28 16:12:34 +0300

    mmm

--- apps/recommend.py
+++ apps/recommend.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import joblib
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
